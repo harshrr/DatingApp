bf97bade88088bb447b52305dd574c043e92237c
harshrr <harshang4u@hotmail.com>
2021-05-27 18:33:40 +0530

    End of Section 12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using AutoMapper;
 
 namespace API.Controllers
 {
@@ -15,8 +16,10 @@ namespace API.Controllers
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
-        public AccountController(DataContext context, ITokenService tokenService)
+        private readonly IMapper _mapper;
+        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         {
+            _mapper = mapper;
             _tokenService = tokenService;
             _context = context;
         }
@@ -24,24 +27,22 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-
             if (await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
 
-            using var hmac = new HMACSHA512();
+            var user = _mapper.Map<AppUser>(registerDto);
 
-            var user = new AppUser
-            {
-                UserName = registerDto.UserName.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key
-            };
+            using var hmac = new HMACSHA512();
+            user.UserName = registerDto.UserName.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return new UserDto {
                 Username= user.UserName,
-                Token= _tokenService.CreateToken(user)
+                Token= _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs
             };
         }
 
@@ -65,7 +66,8 @@ namespace API.Controllers
             return new UserDto {
                 Username= user.UserName,
                 Token= _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                 KnownAs = user.KnownAs
             };
         }
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { retry } from 'rxjs/operators';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,25 +12,57 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {  
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  registerForm: FormGroup;
+  maxDate: Date;
+  validationErrors: string[] = [];
 
-  constructor(private accountService : AccountService,private toastr : ToastrService) { }
+  constructor(private accountService : AccountService,private toastr : ToastrService,
+      private fb:FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.iniatializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18);
+  }
+
+  iniatializeForm() {   
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['',Validators.required],
+      knownAs: ['',Validators.required],
+      dateOfBirth: ['',Validators.required],
+      city: ['',Validators.required],
+      country: ['',Validators.required],
+      password: ['',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]],
+      confirmPassword: ['',[Validators.required,this.matchValues('password')]]
+    });
+  }
+
+  matchValues(matchTo: string) : ValidatorFn {
+    return (control: any) => {
+      return control?.value === control?.parent?.controls[matchTo].value 
+      ? null : {isMatching: true};
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
-    }, error => {
-      this.toastr.error(error.error);      
-      console.log(error);
-    })    
+      this.accountService.register(this.registerForm.value).subscribe(response => {
+        this.router.navigateByUrl('/members');
+      }, error => {
+        this.validationErrors = error;
+      })    
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log("cancelled");
   }
 }
+
+
+// iniatializeForm() {
+//   // this.registerForm = new FormGroup({
+//   //   username: new FormControl('',Validators.required),
+//   //   password: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+//   //   confirmPassword: new FormControl('',[Validators.required,this.matchValues('password')])
+//   // });
+// }
